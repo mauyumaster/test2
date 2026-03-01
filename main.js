dcbb27b8292f7b4950750b52ada98d5be4b514a7
mauyumaster <mauyumaster@gmail.com>
2026-03-01 20:18:38 +0800

    spacedemo

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
-import * as THREE from "three";
-import {OrbitControls} from "three/addons/Addons.js";
+// import * as THREE from "three";
+// import {OrbitControls} from "three/addons/Addons.js";
+import * as THREE from "./Library/three.js-r174/build/three.module"
+console.log(THREE);
 
-const canvas =document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 //scene
-const scene=new THREE.Scene();
-scene.background=new THREE.Color('#F0F0F0');
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
 
 //camera
-const camera=new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 //object
-const geo1=new THREE.CylinderGeometry();
-const mater1=new THREE.MeshNormalMaterial({color:new THREE.Color(0xFFFFFF, 0.5)});
+const geo1 = new THREE.CylinderGeometry();
+const mater1 = new THREE.MeshNormalMaterial({ color: new THREE.Color(0xFFFFFF, 0.5) });
 
-const obj1=new THREE.Mesh(geo1,mater1);
+const obj1 = new THREE.Mesh(geo1, mater1);
 
-const geo2=new THREE.DodecahedronGeometry();
-const mater2=new THREE.MeshDepthMaterial();
+const geo2 = new THREE.DodecahedronGeometry();
+const mater2 = new THREE.MeshDepthMaterial();
 
-const obj2=new THREE.Mesh(geo2,mater2);
- obj2.position.y = -1.5;
+const obj2 = new THREE.Mesh(geo2, mater2);
+obj2.position.y = -1.5;
 
- scene.add(obj2);
- scene.add(obj1);
+scene.add(obj2);
+scene.add(obj1);
 
- //light
-const light=new THREE.SpotLight(0x006796,100);
-light.position.set(1,1,1);
+//light
+const light = new THREE.SpotLight(0x006796, 100);
+light.position.set(1, 1, 1);
 scene.add(light);
 
 //renderer
-const renderer=new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -39,11 +41,11 @@ renderer.setPixelRatio(window.devicePixelRatio);
 // renderer.render(scene, camera);
 
 // orbitcontrol
-const control=new OrbitControls(camera,renderer.domElement);
+const control = new OrbitControls(camera, renderer.domElement);
 control.enableDamping = true;
-control.dampingFactor=0.05;
-control.enableZoom=true;
-control.enablePan=true;
+control.dampingFactor = 0.05;
+control.enableZoom = true;
+control.enablePan = true;
 
 //animation
 function animate() {
